488b637d039f674d6de7d754ed5a4b5b7afc0592
Enrique René Beauxis Reyes <enriquerenebr@gmail.com>
2026-02-19 17:13:48 -0300

    feat: implement payment management system
    - Add PaymentResource for managing payments
    - Create PaymentResource pages (Create, Edit, List, View)
    - Update FinancialDashboard with payment-related widgets and charts
    - Add financial dashboard blade template

---
+++ app/Filament/Resources/PaymentResource/Pages/EditPayment.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Resources\PaymentResource\Pages;
+
+use App\Filament\Resources\PaymentResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditPayment extends EditRecord
+{
+    protected static string $resource = PaymentResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+            Actions\ForceDeleteAction::make(),
+            Actions\RestoreAction::make(),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getSavedNotificationTitle(): ?string
+    {
+        return 'Pagamento atualizado com sucesso!';
+    }
+}
